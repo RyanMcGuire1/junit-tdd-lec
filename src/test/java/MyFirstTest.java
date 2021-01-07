ff0009567c6e1f6842a8ce7fe21350e465033293
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-01-07 11:30:26 -0600

    MyFisrtTest pt1 completed

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -19,4 +19,21 @@ public class MyFirstTest {
         List<String> moreLanguages = new ArrayList<>();
         assertNotSame(languages, moreLanguages);
     }
+
+    @Test
+    public void sameArray(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void statementVerify(){
+        String language = "PHP";
+        assertTrue(language.contains("H")); // use assertTrue for this statement
+        assertFalse(language.contains("J")); // use assertFalse for this statement
+    }
 }
